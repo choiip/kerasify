5cd88b60fd67ebd9dea7a559dde5d14f3442e3ba
arquolo <arquolo@gmail.com>
2018-03-29 20:18:54 +0500

    Minor optimizations of MaxPooling2D

--- src/layers/maxpooling2d.cpp
+++ src/layers/maxpooling2d.cpp
@@ -21,25 +21,26 @@ bool MaxPooling2D::apply(const Tensor& in, Tensor& out) const
 {
     check(in.dims_.size() == 3);
 
-    Tensor tmp{in.dims_[0] / pool_size_y_, in.dims_[1] / pool_size_x_,
-               in.dims_[2]};
+    out.resize(
+        in.dims_[0] / pool_size_y_, in.dims_[1] / pool_size_x_, in.dims_[2]);
+    const auto& out_size = out.dims_;
 
-    for (size_t i = 0; i < tmp.dims_[2]; ++i)
-        for (size_t y2 = 0; y2 < tmp.dims_[0]; ++y2) {
-            const size_t y1 = y2 * pool_size_y_;
-            for (size_t x2 = 0; x2 < tmp.dims_[1]; ++x2) {
-                const size_t x1 = x2 * pool_size_x_;
+    for (size_t y2 = 0; y2 < out_size[0]; ++y2) {
+        const size_t y1 = y2 * pool_size_y_;
+        for (size_t x2 = 0; x2 < out_size[1]; ++x2) {
+            const size_t x1 = x2 * pool_size_x_;
+            for (size_t ch = 0; ch < out_size[2]; ++ch) {
                 float max_val = -std::numeric_limits<float>::infinity();
                 for (size_t py = 0; py < pool_size_y_; ++py)
                     for (size_t px = 0; px < pool_size_x_; ++px) {
-                        const float& pool_val = in(y1 + py, x1 + px, i);
+                        const float& pool_val = in(y1 + py, x1 + px, ch);
                         if (pool_val > max_val)
                             max_val = pool_val;
                     }
-                tmp(y2, x2, i) = max_val;
+                out(y2, x2, ch) = max_val;
             }
         }
-    out = tmp;
+    }
     return true;
 }
 
